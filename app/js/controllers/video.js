6fd1f3251833139e56008cc3d46e8a614ac319c7
joaocampinhos <joao.campinhos@gmail.com>
2014-05-29 18:32:24 +0100

    hum... fuck

--- app/js/controllers/video.js
+++ app/js/controllers/video.js
@@ -5,8 +5,8 @@ angular.module('tvApp').controller('VideoCtrl', function ($scope, VideosService)
   VideosService.get().then(function(data){
     $scope.videos = data;
     //TEST
-    $scope.title = data[0].name;
-    $scope.id = data[0].id;
+    $scope.title = data[1].name;
+    $scope.id = data[1].id;
   });
 
 });
--- app/js/directives/youtube.js
+++ app/js/directives/youtube.js
@@ -3,9 +3,14 @@
 angular.module('tvApp').directive('youtube', function() {
   return function (scope, element) {
     scope.$watch('id', function() {
-      var el = '<iframe src="//www.youtube.com/embed/'+scope.id+'?end=100&modestbranding=0&autoplay=1&controls=0&showinfo=0&enablejsapi=1&rel=0" frameborder="0"></iframe>';
-      console.log(scope.id);
+
+      var el = $('<iframe id="vidz" src="//www.youtube.com/embed/'+scope.id+'?end=100&modestbranding=0&autoplay=1&controls=0&showinfo=0&enablejsapi=1&rel=0" frameborder="0"></iframe>');
+      el.bind( 'onStateChange', function() {
+        console.log( 'User clicked on foo.');
+      });
+
       element.html('').append(el);
+
     });
   };
 });

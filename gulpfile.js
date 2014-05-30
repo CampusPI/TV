b235d0936294cf81c91f29248708c650249cdeb3
joaocampinhos <joao.campinhos@gmail.com>
2014-05-30 19:32:37 +0100

    change port for better local dev

--- gulpfile.js
+++ gulpfile.js
@@ -61,14 +61,14 @@ gulp.task('connect', function () {
     .use(connect.directory('app'));
 
   require('http').createServer(app)
-    .listen(9000)
+    .listen(9069)
     .on('listening', function () {
-        console.log('Started connect web server on http://localhost:9000');
+        console.log('Started connect web server on http://localhost:9069');
       });
 });
 
 gulp.task('serve', ['connect'], function () {
-  require('opn')('http://localhost:9000');
+  require('opn')('http://localhost:9069');
 });
 
 gulp.task('watch', ['connect', 'serve'], function () {

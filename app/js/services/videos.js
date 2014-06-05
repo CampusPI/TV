6ee583fc7e6ada91a77b33f846cdfae1e1a7f186
João Campinhos <joao.campinhos@gmail.com>
2014-06-05 18:56:01 +0100

    add server api support

--- app/js/services/videos.js
+++ app/js/services/videos.js
@@ -1,11 +1,11 @@
 'use strict';
 
-angular.module('tvApp').factory('VideosService', function ($http) {
+angular.module('tvApp').factory('VideosService', function ($http, endpoint) {
   return{
     get: function() {
       return $http({
         method: 'GET',
-        url: 'http://localhost:8080/api/tv/videos'
+        url: endpoint+'/api/tv/videos'
       }).then(function(response) {
         return response.data;
       });

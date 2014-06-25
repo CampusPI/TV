caf46cf50b9c08066df2029681c106db9fd92840
João Campinhos <joao.campinhos@gmail.com>
2014-06-25 11:32:03 +0100

    sidebar

--- app/js/controllers/content.js
+++ app/js/controllers/content.js
@@ -6,6 +6,7 @@ angular.module('tvApp').controller('ContentCtrl', function ($scope, ContentServi
   var d;
 
   var ni = 0;
+  var k = 0;
 
   //$scope.trigger = false;
   var get = function() {
@@ -17,21 +18,52 @@ angular.module('tvApp').controller('ContentCtrl', function ($scope, ContentServi
 
   var prepare = function() {
     $scope.trigger = !$scope.trigger || false;
-    $scope.name = d[0].content[ni].name;
-    $scope.title = d[0].type;
-    $scope.data = d[0].content[ni];
-    delete $scope.data.name;
-    if (n > d.length-1) {
-      n = 0;
-      ni = 0;
-      $timeout(get, 5000);
+    $scope.title = d[n].name;
+    if ($scope.title === 'Ementas') {
+      $scope.name = d[n].content[ni].ementa.Nome;
+      $scope.data = d[n].content[ni].ementa;
+      delete $scope.data.Nome;
+      if (ni > d[n].content.length-2) {
+        if (n > d.length-2) {
+          n = 0;
+        }
+        else {
+          n++;
+        }
+        ni = 0;
+        $timeout(get, 5000);
+      }
+      else {
+        ni++;
+        $timeout(prepare, 5000);
+      }
     }
     else {
-      n++;
-      ni++;
-      $timeout(prepare, 5000);
+      $scope.name = d[n].content[ni].type;
+      $scope.name = d[n].content[ni].type + ' ('+d[n].content[ni].content[k].name+')';
+      $scope.data = d[n].content[ni].content[k].content;
+      console.log(k,d[n].content[ni].content.length-1);
+      if (k > d[n].content[ni].content.length-2) {
+        k = 0;
+        if (ni > d[n].content.length-2) {
+          ni = 0;
+          if (n > d.length-2) {
+            n = 0;
+          }
+          else {
+            n++;
+          }
+        }
+        else {
+          ni++;
+        }
+        $timeout(get, 5000);
+      }
+      else {
+        k++;
+        $timeout(prepare, 5000);
+      }
     }
   };
-
   get();
 });
